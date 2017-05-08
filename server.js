b559395a01d6b5c5f14257198d9de800d59c829b
janstadt <janstadt@gmail.com>
2017-05-07 19:59:27 -0500

    commenting out logs.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ sendPing;
   app.use(bodyParser.text({ type: 'text/html' }));
   
   sendPing = function(url) {
-    console.log("entering sendPing");
+    //console.log("entering sendPing");
     request.get(url, function() {
       console.log('RESPONSE FROM: ' + url);
       pingServer(url);
@@ -18,9 +18,9 @@ sendPing;
 
   pingServer = function(url) {
     var urlToPing = url;
-    console.log("entering pingServer");
+    //console.log("entering pingServer");
     setTimeout(function(){
-      console.log("entering timeout");
+      //console.log("entering timeout");
       var date = new Date(),
           hours = date.getHours();
       if(hours >= 7 && hours <= 24) {
